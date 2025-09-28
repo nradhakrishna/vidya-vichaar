60f1b67d129720c0256839e24c0e86f87fc03602
Naveen Mishra <mveen3@gmail.com>
2025-09-28 17:48:34 +0530

    miner update

--- frontend/src/components/QuestionBoard.js
+++ frontend/src/components/QuestionBoard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Question from './Question';
 
-function QuestionBoard({ questions }) {
+function QuestionBoard({ questions, isTeacher = false, onQuestionUpdated }) {
     if (questions.length === 0) {
         return (
             <div className="question-board">
                 <div className="no-questions">
-                    <p>No questions yet. Be the first to ask something!</p>
+                    <p>No questions yet. {isTeacher ? 'Students will appear here when they ask questions.' : 'Be the first to ask something!'}</p>
                 </div>
             </div>
         );
@@ -17,7 +17,12 @@ function QuestionBoard({ questions }) {
             <h4>Class Questions ({questions.length})</h4>
             <div className="questions-list">
                 {questions.map((question) => (
-                    <Question key={question._id} question={question} />
+                    <Question 
+                        key={question._id} 
+                        question={question} 
+                        isTeacher={isTeacher}
+                        onQuestionUpdated={onQuestionUpdated}
+                    />
                 ))}
             </div>
         </div>
